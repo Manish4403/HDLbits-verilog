bd7a47c732c784b1a76e8d0888797169522a5b1f
Manish sutradhar <105163926+Manish4403@users.noreply.github.com>
2024-08-12 00:01:16 +0530

    Update ps2.v

--- PS2/ps2.v
+++ PS2/ps2.v
@@ -2,38 +2,51 @@ module top_module(
     input clk,
     input [7:0] in,
     input reset,    // Synchronous reset
+    output [23:0] out_bytes,
     output done); //
-	
-    reg [1:0]count;
+
     reg[1:0] state, next_state;
-    parameter s0 = 2'b00, s1 = 2'b01, s2 = 2'b10;
+    reg [23:0] out_b;
+    parameter s0 = 2'b00, s1 = 2'b01, s2 = 2'b10, s3 = 2'b11;
+    
     // State transition logic (combinational)
     always@(*) begin
         case(state)
             s0: next_state = in[3] ? s1 : s0;
-	    s1: next_state = (count < 2'd1) ? s1 : s2; \\counter should count only one state.
-            s2: next_state = in[3] ? s1: s0;
+            s1: next_state = s2; 
+            s2: next_state = s3;
+            s3: next_state = in[3] ? s1: s0;
             default next_state = s0;
         endcase
+    end
+    
+    //Datapath for bytes
+    always@(posedge clk) begin
+        if(state == s0)
+            out_b[23:16] <= in;
+        else if(state == s1)
+            out_b[15:8] <= in;
+        else if(state == s2)
+            out_b[7:0] <= in;
+        else if(state == s3 && !in[3])
+            out_b <= out_b;
+        else
+            out_b[23:16] <= in;
+            
     end
     
     
     // State flip-flops (sequential)
     always@(posedge clk) begin
-        if(reset) begin
+        if(reset)
             state <= s0;
-        end
-        else if(state == s1) begin
-            count <= count + 1'b1;
+        else 
             state <= next_state;
-        end
-        else begin
-            count <= 1'b0;
-            state <= next_state;
-        end
     end
     // Output logic
     
-    assign done = (state == s2)? 1'b1 : 1'b0;
+    assign done = (state == s3)? 1'b1 : 1'b0;
+    
+    assign out_bytes = done ? out_b : 24'd0;
 
 endmodule
